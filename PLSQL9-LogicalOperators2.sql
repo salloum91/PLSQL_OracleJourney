eb71e8e88ec0b6a9d1d791a38997ca2ef7d2229a
salloum91 <aboward70@gmail.com>
2025-06-10 17:24:16 -0700

    PLSQL folder

---
+++ PLSQL9-LogicalOperators2.sql
@@ -0,0 +1,22 @@
+DECLARE 
+   a boolean := true; 
+   b boolean := false; 
+BEGIN 
+   IF (a AND b) THEN 
+      dbms_output.put_line('Line 1 - Condition is true'); 
+   END IF; 
+   IF (a OR b) THEN 
+      dbms_output.put_line('Line 2 - Condition is true'); 
+   END IF; 
+   IF (NOT a) THEN 
+      dbms_output.put_line('Line 3 - a is not true'); 
+   ELSE 
+      dbms_output.put_line('Line 3 - a is true'); 
+   END IF; 
+   IF (NOT b) THEN 
+      dbms_output.put_line('Line 4 - b is not true'); 
+   ELSE 
+      dbms_output.put_line('Line 4 - b is true'); 
+   END IF; 
+END; 
+/
